fdd1cac24794828f0dc8881f514d51f0fe1aaf1b
manuelzzz <manuelss.23.bil@gmail.com>
2024-05-01 11:24:42 -0300

    impl: implementing the Cli struct to recieve the terminal args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(1).expect("no path given");
+
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("Pattern: {:?}, path: {:?}", args.pattern, args.path);
+}
+
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
 }
